386bcba6c1dee6d6965c861f284bc88275684fbf
reccrea_ <wellmoon0@163.com>
2023-09-29 15:42:26 +0800

    chore: vite中less配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import * as path from "path";
 import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import postcssPresetEnv from "postcss-preset-env";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,5 +26,16 @@ export default defineConfig({
 	server: {
 		host: "0.0.0.0",
 		cors: true,
+	},
+	css: {
+		// 预处理器配置项
+		preprocessorOptions: {
+			less: {
+				javascriptEnabled: true,
+			},
+		},
+		postcss: {
+			plugins: [postcssPresetEnv()],
+		},
 	},
 });
