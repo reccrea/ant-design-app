bae18828f41889322a1e843abec8ee18efa57bdc
reccrea_ <wellmoon0@163.com>
2023-09-29 14:28:15 +0800

    ✨ feat:(custom): ant-design-vue组件库引入

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,10 @@
 <template>
-	<div class="home-container"></div>
+	<div class="home-container">
+		<a-button type="primary">Primary Button</a-button>
+		<a-button type="dashed">Dashed Button</a-button>
+		<a-button type="text">Text Button</a-button>
+		<a-button type="link">Link Button</a-button>
+	</div>
 </template>
 
 <script lang="ts" setup></script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import * as path from "path";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +12,16 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "src"),
 		},
 	},
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		Components({
+			resolvers: [
+				AntDesignVueResolver({
+					importStyle: false, // css in js
+				}),
+			],
+		}),
+	],
 	server: {
 		host: "0.0.0.0",
 		cors: true,
